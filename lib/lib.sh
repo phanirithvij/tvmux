0935d1541013ad51665956cedfca931ad413b411
Gareth Davidson <gaz@bitplane.net>
2025-05-23 20:44:35 +0100

    add shell_trap, stolen from rip project

--- lib/lib.sh
+++ lib/lib.sh
@@ -3,6 +3,7 @@
 # Set up base directory if not already set
 BASE_DIR="${BASE_DIR:-$SCRIPT_DIR/.cache}"
 
+source $SCRIPT_DIR/lib/shell.sh
 source $SCRIPT_DIR/lib/cmd.sh
 source $SCRIPT_DIR/lib/log.sh
 source $SCRIPT_DIR/lib/proc.sh
---
+++ lib/shell.sh
@@ -0,0 +1,25 @@
+# Add an exit handler to this pid
+# Usage: shell_trap "command goes here"
+shell_trap() {
+    local name="_SHELL_EXIT_$$"
+    if ! declare -p "$name" &>/dev/null; then
+        declare -g -a "$name"
+        trap __shell_exit EXIT
+    fi
+
+    local -n stack="$name"
+    [[ -n "$1" ]] && stack+=("$1")
+}
+
+# called internally when the shell exits
+__shell_exit() {
+    local name="_SHELL_EXIT_$$"
+
+    # Skip if not defined
+    declare -p "$name" &>/dev/null || return
+
+    local -n stack="$name"
+    for fn in "${stack[@]}"; do
+        eval "$fn" || true
+    done
+}
